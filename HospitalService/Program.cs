666a3aff64bac46e66360051eeb5c3065fcf7b4f
M-Idrees-Khan <muhammadidreeskhan490@gmail.com>
2025-10-06 09:51:11 -0700

    Checking New Branch By adding Few Changes

--- HospitalService/Program.cs
+++ HospitalService/Program.cs
@@ -1,4 +1,5 @@
 using HospitalService.Data;
+using HospitalService.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<HospitalDbContext>(options=>options.UseSqlServer( builder.Configuration.GetConnectionString("HospitalConn")));
+builder.Services.AddScoped<IHospitalService,HospitalServices>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
